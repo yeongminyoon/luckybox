c0bb671bc0bd54dfa4f40ea8a371b41563a6a0fe
youn0 <youn0447@naver.com>
2022-05-01 21:26:48 +0900

    android

---
+++ app/src/main/java/com/example/myapplication/MyApplication.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class MyApplication extends Application {
+    public static final String CHANEL_ID = "luckybox_id";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createChannelNotification();
+    }
+
+    private void createChannelNotification() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANEL_ID, "luckybox",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+    }
+}
