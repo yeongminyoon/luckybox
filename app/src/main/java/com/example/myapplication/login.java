84d01995ce1eeeb04529d563cb41757065520951
youn0 <youn0447@naver.com>
2022-05-16 00:07:24 +0900

    login

--- app/src/main/java/com/example/myapplication/login.java
+++ app/src/main/java/com/example/myapplication/login.java
@@ -1,11 +1,12 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class login extends AppCompatActivity {
 
--- app/src/main/java/com/example/myapplication/register.java
+++ app/src/main/java/com/example/myapplication/register.java
@@ -1,45 +1,83 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseUser;
 
 public class register extends AppCompatActivity {
 
-    private FirebaseAuth mFirebaseAuth; // 파이어베이스 인증처리
-    private DatabaseReference mDatabaseRef;
-    private EditText mEmaileditText, mPassword, mPassword2;
-    private Button msignUpbtn;
+    private static final String TAG = "SignUpActivity";
+    private FirebaseAuth mAuth;
+    private Button sign_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
 
-        mEmaileditText = findViewById(R.id.EmaileditText);
-        mPassword = findViewById(R.id.Password);
-        mPassword2 = findViewById(R.id.Password2);
-        msignUpbtn = findViewById(R.id.signUpbtn);
-
-        msignUpbtn.setOnClickListener(new View.OnClickListener() {
+        sign_up = findViewById(R.id.signUpbtn);
+        sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view)
-            {
-                //회원가입 처리 시작
-
+            public void onClick(View v) {
+                signUp();
             }
         });
-
     }
+
+    // When initializing your Activity, check to see if the user is currently signed in.
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+
+    private void signUp() {
+        // 이메일
+        EditText emailEditText = findViewById(R.id.EmaileditText);
+        String email = emailEditText.getText().toString();
+        // 비밀번호
+        EditText passwordEditText = findViewById(R.id.Password);
+        String password = passwordEditText.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(register.this, login.class);
+                            startActivity(intent);
+                            Toast.makeText(register.this, "등록 완료", Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(register.this, "등록 에러", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    }
+                });
+    }
+
+    Intent intent = getIntent();
 
 }
