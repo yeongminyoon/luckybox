fb5684902fba636b807ce78daf9502e2e34fbe7f
TRANTIENMAN <ankyon96@gmail.com>
2022-05-16 10:46:27 +0900

    Shock screen

--- app/src/main/java/com/example/myapplication/MyApplication.java
+++ app/src/main/java/com/example/myapplication/MyApplication.java
@@ -3,7 +3,10 @@ package com.example.myapplication;
 import android.app.Application;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.Intent;
 import android.os.Build;
+import android.view.View;
+import android.widget.Button;
 
 public class MyApplication extends Application {
     public static final String CHANEL_ID = "luckybox_id";
@@ -19,8 +22,11 @@ public class MyApplication extends Application {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(CHANEL_ID, "luckybox",
                     NotificationManager.IMPORTANCE_DEFAULT);
+
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel);
+
+
         }
     }
 }
