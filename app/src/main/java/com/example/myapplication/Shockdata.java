debc02a874d845bc9effe4aed753f20e8156d353
youn0 <youn0447@gmail.com>
2022-05-23 09:50:40 +0900

    충격내역 및 사진 출력 뒤로가기 버튼 구현

--- app/src/main/java/com/example/myapplication/Shockdata.java
+++ app/src/main/java/com/example/myapplication/Shockdata.java
@@ -3,21 +3,37 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Shockdata extends AppCompatActivity {
 
     TextView tv_shockpart_value;
     TextView tv_shocktime_value;
+    ImageView imageView1;
+    ImageView imageView2;
+    ImageView imageView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +43,13 @@ public class Shockdata extends AppCompatActivity {
         Button btnGetData = findViewById(R.id.btn_get_data);
         tv_shockpart_value = findViewById(R.id.tv_shockpart_value);
         tv_shocktime_value = findViewById(R.id.tv_shocktime_value);
+
+
+        Button btnback = (Button) findViewById(R.id.btn_get_data1);
+        btnback.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Shockdata.this, MainActivity.class);
+                startActivity(intent); }});
 
         btnGetData.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,6 +91,69 @@ public class Shockdata extends AppCompatActivity {
             }
         });
 
+        StorageReference mStorageReference = FirebaseStorage.getInstance().getReference().child("shock_image/test1.jpg");
+
+        try {
+            final File localFile = File.createTempFile("test1", "jpg");
+            mStorageReference.getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(Shockdata.this, "Picture Retrived", Toast.LENGTH_SHORT).show();
+                            Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                            (imageView1 = findViewById(R.id.image_View1)).setImageBitmap(bitmap);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(Shockdata.this, "Error Occurred", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mStorageReference = FirebaseStorage.getInstance().getReference().child("shock_image/test2.jpg");
+
+        try {
+            final File localFile = File.createTempFile("test2", "jpg");
+            mStorageReference.getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(Shockdata.this, "Picture Retrived", Toast.LENGTH_SHORT).show();
+                            Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                            (imageView2 = findViewById(R.id.image_View2)).setImageBitmap(bitmap);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(Shockdata.this, "Error Occurred", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mStorageReference = FirebaseStorage.getInstance().getReference().child("shock_image/test3.jpg");
+
+        try {
+            final File localFile = File.createTempFile("test3", "jpg");
+            mStorageReference.getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(Shockdata.this, "Picture Retrived", Toast.LENGTH_SHORT).show();
+                            Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                            (imageView3 = findViewById(R.id.image_View3)).setImageBitmap(bitmap);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(Shockdata.this, "Error Occurred", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     };
 
